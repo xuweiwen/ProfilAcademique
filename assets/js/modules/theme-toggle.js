6075ea6dd49396316614ff11960608f48f70cc1a
Xu, Wei <xw1224@gmail.com>
2025-09-18 01:39:06 -0400

    Fix bug

--- assets/js/modules/theme-toggle.js
+++ assets/js/modules/theme-toggle.js
@@ -9,12 +9,12 @@ function initThemeToggle() {
 
   const metaThemeColor = document.querySelector('meta[name="theme-color"]');
   const themeToggle = document.getElementById('theme-toggle');
-  const moonIcon = document.getElementById('icon-moon');
-  const sunIcon = document.getElementById('icon-sun');
-  if (!metaThemeColor || !themeToggle || !moonIcon || !sunIcon) return;
+  if (!metaThemeColor || !themeToggle) return;
 
   const setTheme = (theme) => {
     const isLight = theme === 'light';
+    const moonIcon = document.getElementById('icon-moon');
+    const sunIcon = document.getElementById('icon-sun');
     document.documentElement.setAttribute('data-theme', theme);
     localStorage.setItem('theme', theme);
     
